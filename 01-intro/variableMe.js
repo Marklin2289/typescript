a0dddf21f9b68a27157adf5ad79ccfcce243913e
marklin2289 <marklin2289@gmail.com>
2022-12-18 00:36:07 -0800

    initial

---
+++ 01-intro/variableMe.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var greetings = "Mark Lin";
+// let myNum = 6;
+greetings.toLowerCase();
+console.log(" my name is : ".concat(greetings));
+// number
+// let userId: number = 334466;
+var userId = 334466; // better code
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
---
+++ 01-intro/variableMe.ts
@@ -0,0 +1,16 @@
+let greetings: string = "Mark Lin";
+
+// let myNum = 6;
+greetings.toLowerCase();
+console.log(` my name is : ${greetings}`);
+
+// number
+
+// let userId: number = 334466;
+let userId = 334466; // better code
+userId.toFixed();
+
+// boolean
+let isLoggedIn: boolean = false;
+
+export {};
