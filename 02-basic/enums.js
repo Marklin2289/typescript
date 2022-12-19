b651c1069a0eb962c7e03d7a2bdbd89d5dcf16dd
marklin2289 <marklin2289@gmail.com>
2022-12-18 19:20:58 -0800

    different const enum NAME {} compile before and after

--- 02-basic/enums.js
+++ 02-basic/enums.js
@@ -3,4 +3,11 @@
 // const MIDDLE = 1;
 // const WINDOW = 2;
 exports.__esModule = true;
-var hcSeat = "AISLE" /* SeatChoice.AISLE */;
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "AISLE";
+    SeatChoice[SeatChoice["MIDDLE"] = 3] = "MIDDLE";
+    SeatChoice[SeatChoice["WINDOW"] = 4] = "WINDOW";
+    SeatChoice[SeatChoice["FOURTH"] = 5] = "FOURTH";
+})(SeatChoice || (SeatChoice = {}));
+var hcSeat = SeatChoice.AISLE;
--- 02-basic/enums.ts
+++ 02-basic/enums.ts
@@ -2,7 +2,7 @@
 // const MIDDLE = 1;
 // const WINDOW = 2;
 
-const enum SeatChoice {
+const enum SeatChoice { // put const to make it less code in js
   AISLE = "AISLE",
   MIDDLE = 3,
   WINDOW,
@@ -10,5 +10,18 @@ const enum SeatChoice {
 }
 
 const hcSeat = SeatChoice.AISLE;
+
+// js code with const enum ...
+// var hcSeat = "AISLE" /* SeatChoice.AISLE */;
+
+// js code without const
+// var SeatChoice;
+// (function (SeatChoice) {
+//     SeatChoice["AISLE"] = "AISLE";
+//     SeatChoice[SeatChoice["MIDDLE"] = 3] = "MIDDLE";
+//     SeatChoice[SeatChoice["WINDOW"] = 4] = "WINDOW";
+//     SeatChoice[SeatChoice["FOURTH"] = 5] = "FOURTH";
+// })(SeatChoice || (SeatChoice = {}));
+// var hcSeat = SeatChoice.AISLE;
 
 export {};
