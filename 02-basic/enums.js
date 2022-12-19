44cf6b138c3d4a4b250f912957ff25cbbe6695d5
marklin2289 <marklin2289@gmail.com>
2022-12-18 19:17:24 -0800

    enums.ts

---
+++ 02-basic/enums.js
@@ -0,0 +1,6 @@
+"use strict";
+// const AISLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+exports.__esModule = true;
+var hcSeat = "AISLE" /* SeatChoice.AISLE */;
---
+++ 02-basic/enums.ts
@@ -0,0 +1,14 @@
+// const AISLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+
+const enum SeatChoice {
+  AISLE = "AISLE",
+  MIDDLE = 3,
+  WINDOW,
+  FOURTH,
+}
+
+const hcSeat = SeatChoice.AISLE;
+
+export {};
