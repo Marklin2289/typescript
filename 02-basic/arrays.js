cd6cd6e56e6ecd426f43767c945bf93a58d8028d
marklin2289 <marklin2289@gmail.com>
2022-12-18 18:39:46 -0800

    arrays.ts

---
+++ 02-basic/arrays.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    // "" cannot
+    [123, 345, 643],
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
