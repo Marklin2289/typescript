2d916d4e89b38d3737eaa9ee811fecab1a304ae2
marklin2289 <marklin2289@gmail.com>
2022-12-19 18:33:05 -0800

    example

--- 03-purets/dist/index.js
+++ 03-purets/dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("typescript is available");
+// console.log("typescript is available");
+// example
+class User {
+    constructor(email, name) {
+        this.city = "san mateo";
+        this.email = email;
+        this.name = name;
+    }
+}
+const mark = new User("mark@g.com", "mark");
+// mark.city = "Earth";
--- 03-purets/src/index.ts
+++ 03-purets/src/index.ts
@@ -1,1 +1,15 @@
-console.log("typescript is available");
+// console.log("typescript is available");
+
+// example
+class User {
+  email: string;
+  name: string;
+  readonly city: string = "san mateo";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const mark = new User("mark@g.com", "mark");
+// mark.city = "Earth";
