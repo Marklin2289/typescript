a93454004e3eddf44bd7142f8030e4f447ef4b3c
marklin2289 <marklin2289@gmail.com>
2022-12-20 21:34:04 -0800

    narrowing part 5

--- 03-purets/dist/detection.js
+++ 03-purets/dist/detection.js
@@ -46,3 +46,27 @@ function getFood(pet) {
     pet;
     return "bird food";
 }
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    else if (shape.kind === "square") {
+        return shape.side * shape.side;
+    }
+    else {
+        return shape.length * shape.width;
+    }
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultForShape = shape;
+            return _defaultForShape;
+    }
+}
--- 03-purets/src/detection.ts
+++ 03-purets/src/detection.ts
@@ -64,3 +64,43 @@ function getFood(pet: Fish | Bird) {
   pet;
   return "bird food";
 }
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+interface Square {
+  kind: "square";
+  side: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  } else if (shape.kind === "square") {
+    return shape.side * shape.side;
+  } else {
+    return shape.length * shape.width;
+  }
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+    default:
+      const _defaultForShape: never = shape;
+      return _defaultForShape;
+  }
+}
